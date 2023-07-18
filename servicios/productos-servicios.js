06b554eea0328aa1d1fc8f9ea9efd3dc24b532d5
miguel portatil <miguel.bol3456@gmail.com>
2023-07-18 16:25:11 -0500

    se actualiza el host del json

--- servicios/productos-servicios.js
+++ servicios/productos-servicios.js
@@ -1,5 +1,6 @@
 const ListaProductos = () => {
-    return fetch("http://localhost:3000/productos")
+    //return fetch("http://localhost:3000/productos")
+    return fetch("https://miguel-bolanos.github.io/challenge-AluraGeek/db.json")
     .then(respuesta => respuesta.json())
     .catch(error => console.log("error" + error))
 }
